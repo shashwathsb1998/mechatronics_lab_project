ebae32efd7273ee5427649346bdcc8dd8cc693b5
shashwathsb1998 <shashwathsb1998@gmail.com>
2018-11-06 23:54:13 +0530

    Update 4_3button_LED.ino

--- 4_3button_LED/4_3button_LED.ino
+++ 4_3button_LED/4_3button_LED.ino
@@ -1,54 +1,42 @@
-/*
-  Button
-
-  Turns on and off a light emitting diode(LED) connected to digital pin 13,
-  when pressing a pushbutton attached to pin 2.
-
-  The circuit:
-  - LED attached from pin 13 to ground
-  - pushbutton attached to pin 2 from +5V
-  - 10K resistor attached to pin 2 from ground
-
-  - Note: on most Arduinos there is already an LED on the board
-    attached to pin 13.
-
-  created 2005
-  by DojoDave <http://www.0j0.org>
-  modified 30 Aug 2011
-  by Tom Igoe
-
-  This example code is in the public domain.
-
-  http://www.arduino.cc/en/Tutorial/Button
-*/
-
-// constants won't change. They're used here to set pin numbers:
-const int buttonPin = 7; // the number of the pushbutton pin
-const int ledPin =  13;      // the number of the LED pin
-
+const int buttonPin1 = 7; 
+const int ledPin1 =  11;      
+const int buttonPin2 = 8; 
+const int ledPin2 =  12;      
+const int buttonPin3 = 9; 
+const int ledPin3 =  13;      
 // variables will change:
-int buttonState = 0;         // variable for reading the pushbutton status
+int buttonState1 = 0;         // variable for reading the pushbutton status
+int buttonState2 = 0;
+int buttonState3 = 0;
 
 void setup() {
-  // initialize the LED pin as an output:
-  pinMode(ledPin, OUTPUT);
-  // initialize the pushbutton pin as an input:
-  pinMode(buttonPin, INPUT_PULLUP);
+  pinMode(ledPin1, OUTPUT);
+  pinMode(buttonPin1, INPUT_PULLUP);
+  pinMode(ledPin2, OUTPUT);
+  pinMode(buttonPin2, INPUT_PULLUP);
+  pinMode(ledPin3, OUTPUT);
+  pinMode(buttonPin3, INPUT_PULLUP);
 }
 
 void loop() {
   // read the state of the pushbutton value:
-  buttonState = digitalRead(buttonPin);
-
-
-  // check if the pushbutton is pressed. If it is, the buttonState is HIGH:
-  if (buttonState == HIGH) {
-    // turn LED on:
-    digitalWrite(ledPin, LOW);
- 
-    
+  buttonState1 = digitalRead(buttonPin1);
+  buttonState2 = digitalRead(buttonPin2);
+  buttonState3 = digitalRead(buttonPin3);
+  
+  if (buttonState1 == HIGH) {
+    digitalWrite(ledPin1, HIGH);    
   } else {
-    // turn LED off:
-    digitalWrite(ledPin, HIGH);
+    digitalWrite(ledPin1, LOW);
+  }
+  if (buttonState2 == HIGH) {
+    digitalWrite(ledPin2, HIGH);    
+  } else {
+    digitalWrite(ledPin2, LOW);
+  }
+  if (buttonState3 == HIGH) {
+    digitalWrite(ledPin3, HIGH);    
+  } else {
+    digitalWrite(ledPin3, LOW);
   }
 }
